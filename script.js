d17ff883d6dfd9e3586c429a6839175c37bfd113
vedant-simform <vedant@simformsolutions.com>
2023-02-09 19:19:06 +0530

    EOD Commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let obj = {
   Description: "",
 };
 
-function products() {
+function getARR() {
   let a = JSON.parse(localStorage.getItem("ProductData"));
   if (!a) {
     arr = [];
@@ -24,7 +24,13 @@ function products() {
     arr = JSON.parse(localStorage.getItem("ProductData"));
   }
 
-  id++;
+  return arr;
+}
+
+function products() {
+  arr = getARR();
+
+  id = arr.length + 1;
   obj.ID = id;
   obj.Name = document.getElementById("pName").value;
   obj.Image = document.getElementById("pImage").value;
@@ -42,3 +48,68 @@ submit.addEventListener("click", (e) => {
   products();
   e.preventDefault();
 });
+
+function store() {
+  let printValue = document.getElementById("printValue");
+  let printID = document.getElementById("printID");
+  let printPName = document.getElementById("printPName");
+  let printPPrice = document.getElementById("printPPrice");
+  let printPDesc = document.getElementById("printPDesc");
+  let printPImg = document.getElementById("printPImg");
+}
+
+function searchByID() {
+  arr = getARR();
+  store();
+  let tempID = document.getElementById("readID").value;
+  for (let i = 0; i <= arr.length; i++) {
+    if (tempID > arr.length || tempID < 0) {
+      printID.innerHTML = "";
+      printPName.innerHTML = "";
+      printPPrice.innerHTML = "";
+      printPDesc.innerHTML = "";
+      break;
+    }
+    if (arr[i].ID == tempID) {
+      document.getElementById("printValue").style.display = "block";
+      printID.innerHTML = arr[i].ID;
+      printPName.innerHTML = arr[i].Name;
+      printPPrice.innerHTML = arr[i].Price;
+      printPDesc.innerHTML = arr[i].Description;
+    }
+  }
+}
+
+function viewItems() {
+  arr = getARR();
+  console.log(arr);
+  store();
+  document.getElementById("printValue").style.display = "none";
+  // Remember this while sorting
+  for (let i = arr.length - 1; i >= 0; i--) {
+    makeClone();
+
+    document.getElementById("printID").innerHTML = arr[i].ID;
+    document.getElementById("printPName").innerHTML = arr[i].Name;
+    document.getElementById("printPPrice").innerHTML = arr[i].Price;
+    document.getElementById("printPDesc").innerHTML = arr[i].Description;
+    document.getElementById("printValue").style.display = "block";
+    if (i == 0) {
+      document.getElementById("viewAll").disabled = true;
+    }
+  }
+}
+
+function makeClone() {
+  let clone = document.getElementById("printValue").cloneNode(true);
+  document.getElementById("printValue").before(clone);
+}
+
+//   console.log(document.getElementById("parentPrintID").nextSibling.textContent);
+
+function delFun() {
+  let del = document.getElementById("delete");
+  del.addEventListener("click", (e) => {
+    console.log("Hello");
+  });
+}
